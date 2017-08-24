e49873becab863483f7213927518ca9df1979331
santosh <santosh.ece06@gmail.com>
2017-08-24 20:46:58 +0530

    INITIAL Setup.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,4 @@
-let app = angular.module("app", ["ngRoute", "ngMaterial"]);
-
+var app = angular.module("app", ["ngRoute", "ngMaterial"]);
 
 app.config(["$routeProvider", function($routeProvider){
 
